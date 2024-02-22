c19fb6fcae35a6660f86c7882d96eaa971e6b3d9
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:57:18 +0200

    Add project files.

---
+++ Lab1Ex9won7/Program.cs
@@ -0,0 +1,21 @@
+﻿//Scrieti un program care interschimba valoarea a doua variabile intregi.
+
+
+Console.Write("Introduce primul numar: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Introduce al doilea numar: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int tem;
+
+tem = num1;
+num1 = num2;
+num2 = tem;
+
+Console.WriteLine("\n Dupa interschimbarea: ");
+Console.WriteLine("\n primul numar: " + num1);
+Console.WriteLine("\n Al doilea numar: " + num2);
+
+Console.ReadLine();
+
